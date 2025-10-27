344506363c741cff01bf1fff9e5cf0054b33883f
Shahnur Milon <109983076+SHAHNUR00@users.noreply.github.com>
2025-10-27 12:21:36 +0600

    Create input.h

---
+++ input.h
@@ -0,0 +1,11 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+
+void studentInput();
+void departmentInput();
+void courseInput();
+void enrollInput();
+void changeEntryForStudent();
+void changeEntryForDept();
+
+#endif
